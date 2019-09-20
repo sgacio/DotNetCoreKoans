b5acdcbb465f2b5e047b87a694936733d821f06f
Stephen Gacio <sgacio@mail.usf.edu>
2019-09-20 09:58:43 -0400

    completed koans; reached enlightenment

--- Koans/AboutLinq.cs
+++ Koans/AboutLinq.cs
@@ -7,74 +7,74 @@ using Xunit;
 
 namespace DotNetCoreKoans.Koans
 {
-    public class AboutLinq : Koan
+  public class AboutLinq : Koan
+  {
+    /*
+     *LINQ (Language Integrated Query).
+     * LINQ provides language-level querying capabilities and a higher-order function API to C# and VB as a way to write expressive, declarative code.
+     * All LINQ query operations consist of three distinct actions:
+
+        1. Obtain the data source.
+
+        2. Create the query.
+
+        3. Execute the query.
+     */
+
+    [Step(1)]
+    public void FilterArrayData()
     {
-        /*
-         *LINQ (Language Integrated Query).
-         * LINQ provides language-level querying capabilities and a higher-order function API to C# and VB as a way to write expressive, declarative code.
-         * All LINQ query operations consist of three distinct actions:
+      //This sample uses where to find all elements of an array less than 5.
 
-            1. Obtain the data source.
+      // The Three Parts of a LINQ Query:
 
-            2. Create the query.
+      //  1. Data source.
+      int[] numbers = { 5, 1, 9, 8, 6, 7 };
 
-            3. Execute the query.
-         */
+      // 2. Query creation.
+      var lowNums =
+          from n in numbers
+          where n < 5
+          select n;
 
-        [Step(1)]
-        public void FilterArrayData()
-        {
-            //This sample uses where to find all elements of an array less than 5.
-
-            // The Three Parts of a LINQ Query:
-
-            //  1. Data source.
-            int[] numbers = { 5, 1, 9, 8, 6, 7 };
-
-            // 2. Query creation.
-            var lowNums =
-                from n in numbers
-                where n < 5
-                select n;
-
-            // 3. Query execution.
-            Assert.Equal(FILL_ME_IN, lowNums.Count());
-        }
-
-        [Step(2)]
-        public void PutYourDataInOrderUsingOrderBy()
-        {
-
-            string[] customers = { "John", "Bill", "Maria", "George", "Anna" };
-
-            var orderedCustomers =
-                 from cust in customers
-                 orderby cust ascending //You can also use descending here for reverse order.
-                 select cust;
-
-            Assert.Equal(FILL_ME_IN, orderedCustomers.First());
-            Assert.Equal(FILL_ME_IN, orderedCustomers.Last());
-        }
-
-        [Step(3)]
-        public void GetJustTheDataYouWantUsingTake()
-        {
-
-            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-
-             //Get just the 3 first numbers.
-            var first3Numbers = numbers.Take(3);
-
-            Assert.Equal(FILL_ME_IN, first3Numbers.Count());
-        }
-
-        [Step(4)]
-        public void UseAnyToDoSmartChecksOnYourData()
-        {
-            string[] words = { "believe", "relief", "receipt", "field" };
-
-            bool iAfterE = words.Any(w => w.Contains("ei")); //Check if any of your words contain 'ei'
-            Assert.Equal(FILL_ME_IN, iAfterE);
-        }
+      // 3. Query execution.
+      Assert.Equal(1, lowNums.Count());
     }
+
+    [Step(2)]
+    public void PutYourDataInOrderUsingOrderBy()
+    {
+
+      string[] customers = { "John", "Bill", "Maria", "George", "Anna" };
+
+      var orderedCustomers =
+           from cust in customers
+           orderby cust ascending //You can also use descending here for reverse order.
+           select cust;
+
+      Assert.Equal("Anna", orderedCustomers.First());
+      Assert.Equal("Maria", orderedCustomers.Last());
+    }
+
+    [Step(3)]
+    public void GetJustTheDataYouWantUsingTake()
+    {
+
+      int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+      //Get just the 3 first numbers.
+      var first3Numbers = numbers.Take(3);
+
+      Assert.Equal(3, first3Numbers.Count());
+    }
+
+    [Step(4)]
+    public void UseAnyToDoSmartChecksOnYourData()
+    {
+      string[] words = { "believe", "relief", "receipt", "field" };
+
+      bool iAfterE = words.Any(w => w.Contains("ei")); //Check if any of your words contain 'ei'
+      Assert.Equal(true, iAfterE);
+    }
+  }
 }
--- Koans/PathToEnlightenment.cs
+++ Koans/PathToEnlightenment.cs
@@ -3,33 +3,47 @@ using DotNetCoreKoans.Engine;
 
 namespace DotNetCoreKoans.Koans
 {
-    public class PathToEnlightenment : Path
+  public class PathToEnlightenment : Path
+  {
+    public PathToEnlightenment()
     {
-        public PathToEnlightenment()
-        {
-           Types = new Type[] {
+      Types = new Type[] {
                 // START: Adventure Mode
                 typeof(AboutAsserts),
+                //passed
                 typeof(AboutNull),
+                //passed
                 typeof(AboutArrayAssignment),
+                //passed
                 typeof(AboutStrings),
+                //passed
                 typeof(AboutFloats),
+                //passed
                 typeof(AboutControlStatements),
+                //passed
                 typeof(AboutArrays),
+                //passed
                 typeof(AboutMethods),
+                //passed
                 // END: Adventure Mode
 
                 // START: Explorer mode
                 typeof(AboutDelegates),
+                //passed 
                 typeof(AboutLambdas),
+                //passed
                 typeof(AboutLinq),
+                //passed
                 typeof(AboutInheritance),
+                //passed
                 // END: Explorer Mode
 
                 // START: Epic mode
                 typeof(AboutGenericContainers),
+                //passed
                 typeof(AboutBitwiseAndShiftOperator),
+                //passed
                 };
-        }
     }
+  }
 }
